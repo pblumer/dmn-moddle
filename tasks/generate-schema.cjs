15f88b28585efe6388f1ba574b8b634c26963bf9
pblumer <34165194+pblumer@users.noreply.github.com>
2026-09-08 20:43:12 +0200

    fix: normalize UML model root for DMN 1.5 XMI

--- tasks/generate-schema.cjs
+++ tasks/generate-schema.cjs
@@ -9,9 +9,16 @@ const {
   transformDMNDI
 } = require('./transforms/index.cjs');
 
+function normalizeUmlModelRoot(xmi) {
+  return xmi
+    .replace('<uml:Model ', '<uml:Package ')
+    .replace('</uml:Model>', '</uml:Package>');
+}
+
 async function generateSchema(files) {
   for (const file of files) {
     const {
+      normalize,
       options,
       source,
       target,
@@ -19,7 +26,13 @@ async function generateSchema(files) {
       transformOptions
     } = file;
 
-    const parsed = await parseFile(fs.readFileSync(source, 'utf8'), options);
+    let sourceContents = fs.readFileSync(source, 'utf8');
+
+    if (normalize) {
+      sourceContents = normalize(sourceContents);
+    }
+
+    const parsed = await parseFile(sourceContents, options);
 
     const transformed = await transform(parsed, transformOptions);
 
@@ -32,6 +45,9 @@ generateSchema([
     source: 'resources/dmn/xmi/DMN13.xmi',
     target: 'resources/dmn/json/dmn13.json',
     transform: transformDMN,
+    transformOptions: {
+      packageName: 'DMN'
+    },
     options: {
       clean: true,
       prefixNamespaces: {
@@ -47,6 +63,9 @@ generateSchema([
     source: 'resources/dmn/xmi/DMNDI13.xmi',
     target: 'resources/dmn/json/dmndi13.json',
     transform: transformDMNDI,
+    transformOptions: {
+      packageName: 'DMNDI'
+    },
     options: {
       clean: true,
       prefixNamespaces: {
@@ -60,8 +79,10 @@ generateSchema([
     target: 'resources/dmn/json/dmn15.json',
     transform: transformDMN,
     transformOptions: {
+      packageName: 'DMN',
       xsdFile: 'resources/dmn/xsd/DMN15.xsd'
     },
+    normalize: normalizeUmlModelRoot,
     options: {
       clean: true,
       prefixNamespaces: {
@@ -78,8 +99,10 @@ generateSchema([
     target: 'resources/dmn/json/dmndi15.json',
     transform: transformDMNDI,
     transformOptions: {
+      packageName: 'DMNDI',
       xsdFile: 'resources/dmn/xsd/DMNDI15.xsd'
     },
+    normalize: normalizeUmlModelRoot,
     options: {
       clean: true,
       prefixNamespaces: {
