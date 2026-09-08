c83309ae75ce2ea5bfdfab4b5964ed0e3daa1b2c
pblumer <34165194+pblumer@users.noreply.github.com>
2026-09-08 20:39:07 +0200

    feat: generate DMN 1.5 moddle descriptors

--- tasks/generate-schema.cjs
+++ tasks/generate-schema.cjs
@@ -10,20 +10,21 @@ const {
 } = require('./transforms/index.cjs');
 
 async function generateSchema(files) {
-  files.forEach(async file => {
+  for (const file of files) {
     const {
       options,
       source,
       target,
-      transform
+      transform,
+      transformOptions
     } = file;
 
     const parsed = await parseFile(fs.readFileSync(source, 'utf8'), options);
 
-    const transformed = await transform(parsed);
+    const transformed = await transform(parsed, transformOptions);
 
     fs.writeFileSync(target, JSON.stringify(transformed, null, 2));
-  });
+  }
 }
 
 generateSchema([
@@ -46,6 +47,39 @@ generateSchema([
     source: 'resources/dmn/xmi/DMNDI13.xmi',
     target: 'resources/dmn/json/dmndi13.json',
     transform: transformDMNDI,
+    options: {
+      clean: true,
+      prefixNamespaces: {
+        'DC': 'dc',
+        'DI': 'di'
+      }
+    }
+  },
+  {
+    source: 'resources/dmn/xmi/DMN15.xmi',
+    target: 'resources/dmn/json/dmn15.json',
+    transform: transformDMN,
+    transformOptions: {
+      xsdFile: 'resources/dmn/xsd/DMN15.xsd'
+    },
+    options: {
+      clean: true,
+      prefixNamespaces: {
+        'DC': 'dc',
+        'DI': 'di',
+        'http://www.omg.org/spec/BMM/20130801/BMM.xmi': 'bmm',
+        'http://www.omg.org/spec/BPMN/20100501/BPMN20.cmof': 'bpmn',
+        'https://www.omg.org/spec/DMN/20230324/DMNDI15.xmi': 'dmndi'
+      }
+    }
+  },
+  {
+    source: 'resources/dmn/xmi/DMNDI15.xmi',
+    target: 'resources/dmn/json/dmndi15.json',
+    transform: transformDMNDI,
+    transformOptions: {
+      xsdFile: 'resources/dmn/xsd/DMNDI15.xsd'
+    },
     options: {
       clean: true,
       prefixNamespaces: {
@@ -78,4 +112,7 @@ generateSchema([
       }
     }
   }
-]);
+]).catch(error => {
+  console.error(error);
+  process.exitCode = 1;
+});
